cf035b16a2c089228442a125a1bcc3ea91c473ab
Swapnil Sharma <swapnilsharma1163@gmail.com>
2026-04-26 12:38:52 +0530

    Initial commit

---
+++ src/test/java/org/example/customersupportagent/CustomerSupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.customersupportagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
